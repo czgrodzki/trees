2c81d9e38374407ab0d09bfb9501d0490113bc2a
Bartosz Czerwonogrodzki <bczerwonogrodzki@atlassian.com>
2022-09-14 22:07:46 +0200

    Update app.

--- src/main/java/com/treesviewer/trees/TreesApplication.java
+++ src/main/java/com/treesviewer/trees/TreesApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class TreesApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(TreesApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(TreesApplication.class, args);
+    }
 
 }
